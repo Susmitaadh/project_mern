8c6fced7f2d19c3ccbc33167c3656bea75445f86
Susmita <190599680+Susmitaadh@users.noreply.github.com>
2026-09-16 15:07:22 +0545

    Multer filr filter

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,11 +1,23 @@
 import multer from "multer";
 import fs from "fs";
+import path from "path";
 import { Request } from "express";
+import AppError from "../utils/appError.utils";
 
 const uploader = () => {
   //* create upload folder if not exists
   const folder = "uploads/";
   const fileSize = 5 * 1024 * 1024; // 5MB max file size
+
+  const allowed_exts = [".png", ".jpg", ".jpeg", ".webp", ".svg", ".pdf"];
+  const allowed_mime_types = [
+    "image/png",
+    "image/jpg",
+    "image/jpeg",
+    "image/svg+xml",
+    "application/pdf",
+  ];
+
   if (!fs.existsSync(folder)) {
     fs.mkdirSync(folder);
   }
@@ -21,11 +33,31 @@ const uploader = () => {
     },
   });
 
+  const fileFilter = (
+    _: Request,
+    file: Express.Multer.File,
+    cb: multer.FileFilterCallback,
+  ) => {
+    //* check if file extension is allowed
+    if (!allowed_exts.includes(path.extname(file.originalname).toLowerCase())) {
+      cb(new AppError(`only ${allowed_exts.join(",")} type is allowed`, 400));
+      return;
+    }
+
+    //* check if file mimetype is allowed
+    if (!allowed_mime_types.includes(file.mimetype)) {
+      cb(new AppError(`invalid file type, only image & pdf is allowed`, 400));
+      return;
+    }
+    cb(null, true);
+  };
+
   //* multer upload instance
   const upload = multer({
     storage: storage,
+    fileFilter: fileFilter,
     limits: {
-        fileSize: fileSize,
+      fileSize: fileSize,
     },
   });
 
