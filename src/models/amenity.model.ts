83fdb694d5d180e7dee50ba70dc3120f87936033
Susmita <190599680+Susmitaadh@users.noreply.github.com>
2026-09-17 13:28:07 +0545

    Amenity model

--- src/models/amenity.model.ts
+++ src/models/amenity.model.ts
@@ -1,1 +1,42 @@
-// name, logo/icon, description, user who created
+import mongoose from "mongoose";
+
+interface IAmenityDocument extends Document {
+  name: string;
+  description: string;
+  logo: string;
+  user: mongoose.Types.ObjectId;
+}
+
+const amenitySchema = new mongoose.Schema<IAmenityDocument>(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Name is required"],
+      unique: [true, "Amenity already exists"],
+      minLength: [3, "At least 3 characters are required"],
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "Description is required"],
+      minLength: [10, "At least 10 characters are required"],
+    },
+
+    logo: {
+      type: String,
+      required: [true, "Logo is required"],
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "User is required"],
+      ref: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const Amenity = mongoose.model("amenity", amenitySchema);
+export default Amenity;
